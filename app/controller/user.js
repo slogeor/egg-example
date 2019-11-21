b4ae323478d4fc5b1b0e92fee8281687b17520c4
xiao.geng <xiao.geng@wormpex.com>
2019-11-21 17:25:20 +0800

    sql 的 增删改查

--- app/controller/user.js
+++ app/controller/user.js
@@ -5,9 +5,19 @@ const Controller = require('egg').Controller;
 class UserController extends Controller {
   async info() {
     const ctx = this.ctx;
-    const userId = ctx.params.id;
-    const user = await ctx.service.user.find(userId);
-    ctx.body = user;
+    const type = ctx.request.query.type;
+    let msg = 'hello world';
+    if (type === 'query') {
+      msg = await ctx.service.user.query();
+    } else if (type === 'update') {
+      msg = await ctx.service.user.update();
+    } else if (type === 'delete') {
+      msg = await ctx.service.user.delete();
+    } else if (type === 'add') {
+      msg = await ctx.service.user.add();
+    }
+
+    ctx.body = msg;
   }
 }
 
--- app/service/user.js
+++ app/service/user.js
@@ -3,9 +3,34 @@
 const Service = require('egg').Service;
 
 class UserService extends Service {
-  async find() {
-    // 假如 我们拿到用户 id 从数据库获取用户详细信息
-    const user = await this.app.mysql.get('users', { id: 11 });
+  // 查询
+  async query() {
+    const user = await this.app.mysql.get('user', { userId: 1111 });
+    return { user };
+  }
+
+  // 新增
+  async add() {
+    const user = await this.app.mysql.insert('user', { userId: 222, userName: 'tom' });
+    return { user };
+  }
+
+  // 更新
+  async update() {
+    const row = {
+      userName: '111Jam',
+    };
+    const options = {
+      where: {
+        userId: 1111,
+      },
+    };
+    const user = await this.app.mysql.update('user', row, options);
+    return { user };
+  }
+  // 更新
+  async delete() {
+    const user = await this.app.mysql.delete('user', { userId: 222 });
     return { user };
   }
 }
